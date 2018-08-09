281f0e8a59b0e4041920a17d08e8e5ad43a4c0f9
spoofhopper <morgan.holland@gmail.com>
2018-08-09 07:18:42 -0700

    added delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,5 +48,16 @@ class PostsController < ApplicationController
     end
   end
 
+def destroy
+  @post = Post.find(params[:id])
+
+  if @post.destroy
+    flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+    redirect_to posts_path
+  else
+    flash.now[:alert] = "There was an error deleting the post."
+    render :show
+  end
+end
 
 end
